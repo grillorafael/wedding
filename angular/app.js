bb907fe490ca8a30b418eed9e8be9be8c8ca47df
Rafael Grillo <grillorafael@gmail.com>
2015-10-25 16:34:29 +0000

    test

--- angular/app.js
+++ angular/app.js
@@ -17,41 +17,38 @@ angular.module('wedding', [])
 
         $scope.submit = function() {
             $http({
-                url: 'https://api:key-0qsp-amfc75mfw-u0ggm-grp0b4jmis1@api.mailgun.net/v3/rgrillo.com/messages',
+                url: 'http://rgrillo/mariaerafael/process.php',
                 method: 'POST',
-                headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
                 data: $.param({
-                    from: "Rafael Grillo <grillorafael@gmail.com>",
-                    to: $scope.rsvp.email,
-                    subject: "Confirmação de " + $scope.rsvp.name,
-                    text: "O convidado " + $scope.rsvp.name + " confirmou a presença de " + $scope.rsvp.guests + " convidados."
+                    name: $scope.rsvp.name,
+                    guests: $scope.rsvp.guests
                 })
             });
         };
 
         $http.get('data/slides.json').
-            success(function(data) {
-                $scope.slides = data;
+        success(function(data) {
+            $scope.slides = data;
 
-                $scope.slidesRefresh();
-                $scope.reveal();
-            }).
-            error(function(data) {
-            });
+            $scope.slidesRefresh();
+            $scope.reveal();
+        }).
+        error(function(data) {});
 
         $http.get('data/vips.json').
-            success(function(data) {
-                $scope.vips = data;
-                $scope.reveal();
-            }).
-            error(function(data) {
-            });
+        success(function(data) {
+            $scope.vips = data;
+            $scope.reveal();
+        }).
+        error(function(data) {});
 
         $http.get('data/gallery.json').
-            success(function(data) {
-                $scope.gallery = data;
-                $scope.reveal();
-            }).
-            error(function(data) {
-            });
+        success(function(data) {
+            $scope.gallery = data;
+            $scope.reveal();
+        }).
+        error(function(data) {});
     }]);
--- process.php
+++ process.php
@@ -1,1 +1,35 @@
 <?php
+
+$from = 'Rafael Grillo <grillorafael@gmail.com>';
+$to = 'marialowen@gmail.com';
+$name = $_POST['name'];
+$guests = $_POST['guests'];
+$subject = "Confirmação do convidado - $name";
+$message = "O convidado $name confirmou $guests convidados";
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+curl_setopt($ch, CURLOPT_USERPWD, 'api:key-0qsp-amfc75mfw-u0ggm-grp0b4jmis1');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+$plain = strip_tags(br2nl($message));
+
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($ch, CURLOPT_URL, 'https://api.mailgun.net/v3/rgrillo.com/messages');
+curl_setopt($ch, CURLOPT_POSTFIELDS, array('from' => 'support@rgrillo.com',
+    'to' => $to,
+    'subject' => $subject,
+    'html' => $message,
+    'text' => $plain));
+
+$j = json_decode(curl_exec($ch));
+
+$info = curl_getinfo($ch);
+
+if($info['http_code'] != 200)
+    error("Fel 313: Vänligen meddela detta via E-post till support@".DOMAIN);
+
+curl_close($ch);
+
+return json_encode($j);
